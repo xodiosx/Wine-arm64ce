4595526532bef1de69c4117438c34648677c52ba
Paul Gofman <pgofman@codeweavers.com>
2025-11-21 18:51:51 -0600

    ntdll: Support iterative barrier usage without re-initialization.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=59017
    (cherry picked from commit b90044a3d5618a227ce620e3f0386d2ae79926f0)
    (preemptive cherry-pick)

--- dlls/ntdll/sync.c
+++ dlls/ntdll/sync.c
@@ -1382,6 +1382,7 @@ struct barrier_impl
     LONG total_thread_count;
     volatile LONG reached_thread_count;
     volatile LONG structure_lock_count;
+    volatile LONG waiting_thread_count;
     volatile LONG wait_barrier_complete;
 };
 
@@ -1401,6 +1402,7 @@ NTSTATUS WINAPI RtlInitBarrier( RTL_BARRIER *barrier, LONG thread_count, LONG sp
     b->spin_count = spin_count;
     b->reached_thread_count = 0;
     b->structure_lock_count = 0;
+    b->waiting_thread_count = 0;
     b->wait_barrier_complete = 0;
     return STATUS_SUCCESS;
 }
@@ -1445,11 +1447,28 @@ BOOLEAN WINAPI RtlBarrier( RTL_BARRIER *barrier, ULONG flags )
     if (flags & ~0x10000 && !once++) FIXME( "Unknown flags %#lx.\n", flags );
     if (!barrier) return FALSE;
 
-    if (ReadAcquire( &b->reached_thread_count ) >= b->total_thread_count) return TRUE;
-
     /* Incrementing reached_thread_count may trigger RTL_BARRIER data desrtuction from another thread,
      * so lock RtlDeleteBarrier with waiting_thread_count before that. */
     if (flags & 0x10000) InterlockedIncrement( &b->structure_lock_count );
+
+    /* On Windows the long wait doesn't consume CPU with any spin count, so probably the spin count
+     * is limited or not used at all. */
+    spin_count = min( 2000, (unsigned int)b->spin_count );
+
+    /* Wait for previous wait iteration to complete. */
+    count = 0;
+    while (ReadAcquire( &b->reached_thread_count ) == b->total_thread_count)
+    {
+        if (count < spin_count)
+        {
+            ++count;
+            YieldProcessor();
+            continue;
+        }
+        RtlWaitOnAddress( (void *)&b->reached_thread_count, &b->total_thread_count,
+                          sizeof(b->reached_thread_count), NULL );
+    }
+    InterlockedIncrement( &b->waiting_thread_count );
     if (InterlockedIncrement( &b->reached_thread_count ) == b->total_thread_count)
     {
         WriteRelease( &b->wait_barrier_complete, 1 );
@@ -1457,9 +1476,6 @@ BOOLEAN WINAPI RtlBarrier( RTL_BARRIER *barrier, ULONG flags )
         ret = TRUE;
         goto done;
     }
-    /* On Windows the long wait doesn't consume CPU with any spin count, so probably the spin count
-     * is limited or not used at all. */
-    spin_count = min( 2000, (unsigned int)b->spin_count );
     count = 0;
     while (ReadAcquire( &b->reached_thread_count ) < b->total_thread_count )
     {
@@ -1473,6 +1489,13 @@ BOOLEAN WINAPI RtlBarrier( RTL_BARRIER *barrier, ULONG flags )
     }
 
 done:
+    if (!InterlockedDecrement( &b->waiting_thread_count ))
+    {
+        WriteRelease( &b->wait_barrier_complete, 0 );
+        WriteRelease( &b->reached_thread_count, 0 );
+        RtlWakeAddressAll( (const void *)&b->reached_thread_count );
+    }
+
     if (flags & 0x10000 && !InterlockedDecrement( &b->structure_lock_count ))
     {
         /* Now RTL_BARRIER structure contents may become invalid. Signaling on its address should be fine, the worst
