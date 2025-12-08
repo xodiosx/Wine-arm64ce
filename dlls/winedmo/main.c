2e037219f29de6fae7a6203bbdb13f7416ee6bbc
Charlotte Pabst <cpabst@codeweavers.com>
2025-12-08 12:04:39 +0100

    Revert "winedmo: Use the stream context to cache stream chunks."
    This reverts commit 3af2cc5525597ed7391abe8d56a6a1dfe41ca54c.
    
    CW-Bug-Id: #24703

--- dlls/winedmo/main.c
+++ dlls/winedmo/main.c
@@ -30,12 +30,11 @@ static struct stream_context *stream_context_create( struct winedmo_stream *stre
 {
     struct stream_context *context;
 
-    if (!(context = malloc( sizeof(*context) + 0x10000 ))) return NULL;
+    if (!(context = malloc( 0x10000 ))) return NULL;
     context->stream = (UINT_PTR)stream;
     context->length = stream_size;
     context->position = 0;
-    context->capacity = 0x10000;
-    context->size = 0;
+    context->buffer_size = 0x10000 - offsetof(struct stream_context, buffer);
 
     return context;
 }
