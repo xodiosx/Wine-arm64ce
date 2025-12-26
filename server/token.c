63d3324b06c60543df35d50aa7b7018cf9f0dbdc
Ziad <160241605+ziad9267@users.noreply.github.com>
2025-12-26 18:11:28 +0200

    server: Fix machine-id path

--- server/token.c
+++ server/token.c
@@ -221,7 +221,7 @@ void init_user_sid(void)
     size_t n;
     FILE *f;
 
-    f = fopen( "/etc/machine-id", "r" );
+    f = fopen( "/data/data/com.termux/files/usr/etc/machine-id", "r" );
     if (!f)
     {
         fprintf( stderr, "Failed to open /etc/machine-id, error %s.\n", strerror( errno ));
